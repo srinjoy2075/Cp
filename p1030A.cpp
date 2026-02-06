f6a6dee5a6136aca6b5324905adfc627f9da8ef4
srinjoy2075 <iamsrinjoy03@gmail.com>
2026-02-06 09:57:30 +0530

    Add files via upload

---
+++ p1030A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+    while (n--) {
+        cin >> x;
+        if (x == 1) {
+            cout << "HARD";
+            return 0;
+        }
+    }
+    cout << "EASY";
+    return 0;
+}
---
+++ p785A.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int faces = 0;
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s == "Tetrahedron") faces += 4;
+        else if (s == "Cube") faces += 6;
+        else if (s == "Octahedron") faces += 8;
+        else if (s == "Dodecahedron") faces += 12;
+        else if (s == "Icosahedron") faces += 20;
+    }
+
+    cout << faces << endl;
+    return 0;
+}
